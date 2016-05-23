db757b5d57edf5456d32386df5be58138ee5f5fb
littlebirch <fyl1011@163.com>
2016-05-23 22:23:46 +0800

    The second version,program will receive two argument in command line.

--- spider.py
+++ spider.py
@@ -7,10 +7,16 @@ This program will connect to a web server,send a request and waiting
 to receive a reply,then print the reply out to standard output
 ####################################################################
 """
+import sys
+import string
 from socket import *
 
-HOST = '192.168.1.1'
-PORT = 80
+nargv = len(sys.argv)
+if nargv != 3:
+	print 'Usage %s host port' %sys.argv[0]
+	quit()
+HOST = sys.argv[1]
+PORT = string.atoi(sys.argv[2])
 BUFSIZ = 1024
 ADDR = (HOST,PORT)
 User_Agent = 'Spider/0.01'
@@ -21,12 +27,10 @@ tcpCliSock.connect(ADDR)
 
 data = 'Get / %s\r\nHost: %s\r\nUser_Agent: %s\r\n\r\n' %(Version,HOST,User_Agent)
 
-#print data
 tcpCliSock.send(data)
 while True:
 	data = tcpCliSock.recv(BUFSIZ)
 	print data
 	if not data:
 		break
-#print data
 tcpCliSock.close()
